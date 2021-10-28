da57acc151c330ba11a0cf0be1095a1226f27cd4
baolong24 <1667375308@qq.com>
2021-10-28 06:00:30 +0000

    TMP

--- product_prop.mk
+++ product_prop.mk
@@ -37,7 +37,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
-    debug.sf.latch_unsignaled=1 \
+    debug.sf.latch_unsignaled=0 \
     debug.sf.enable_hwc_vds=1 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
